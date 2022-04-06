7ab55a0caf59df592c29c56a4d5902b7c799ca00
SangGyu Park <gyuya07@gmail.com>
2022-04-06 16:49:03 +0900

    테스트2

--- mobile/bridge/sample.js
+++ mobile/bridge/sample.js
@@ -1,5 +1,8 @@
 const showKeypad = document.getElementById('showKeypad');
 const closeKeypad = document.getElementById('closeKeypad');
+const openCam = document.getElementById('openCam');
 
 showKeypad.addEventListener('click', _=> alert('showKeypad'));
 closeKeypad.addEventListener('click', _=> alert('closeKeypad'));
+openCam.addEventListener('click', _=> window.android2.openPhoneCamera());
+
