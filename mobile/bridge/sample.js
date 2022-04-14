5f490d0333a766a40f2633a84e4667214ac0f461
SangGyu Park <gyuya07@gmail.com>
2022-04-14 20:26:30 +0900

    안드로이드 테스트

--- mobile/bridge/sample.js
+++ mobile/bridge/sample.js
@@ -5,14 +5,22 @@ const showKpad = document.getElementById('showKeypad');
 const closeKpad = document.getElementById('closeKeypad');
 const openCam = document.getElementById('openCamera');
 const openBrws = document.getElementById('openImageBrowser');
-const getMinfo = document.getElementById('getMobileInfo');
+const openWeb = document.getElementById('openWebPage');
+
+const getMinfo1 = document.getElementById('getMobileInfo1');
+const getMinfo2 = document.getElementById('getMobileInfo2');
+const getMinfo3 = document.getElementById('getMobileInfo3');
 
 
 showKpad.addEventListener('click', _=> showKeypad());
 closeKpad.addEventListener('click', _=> closeKeypad());
 openCam.addEventListener('click', _=> openCamera() );
 openBrws.addEventListener('click', _=> OpenImageBrowser() );
-getMinfo.addEventListener('click', _=> getMobileInfo());
+openWeb.addEventListener('click', _=> openWebPage());
+
+getMinfo1.addEventListener('click', _=> getMobileInfo1());
+getMinfo2.addEventListener('click', _=> getMobileInfo2());
+getMinfo3.addEventListener('click', _=> getMobileInfo3());
 
 function openCamera() {
     if(ua.indexOf("Android") > 0) {
@@ -70,21 +78,76 @@ function OpenImageBrowser() {
     }   
 }
 
-
-function getMobileInfo() {
+function openWebPage() {
     var info = '';
     if(ua.indexOf("Android") > 0) {
-       info = window.bridge.setCommonData('GetMobileInfo','') ; 
-       alert(info);
+       info = window.bridge.setCommonData('OpenWebPage','http://www.naver.com') ; 
+      // alert(info);
        return info;
     }
     else if(ua.indexOf("iPhone") > 0) {
-        info = window.location = "GetMobileInfo://?"; 
-        alert(info);
+        info = window.location = "OpenWebPage://?"; 
+        //alert(info);
         return  info;//+data;        
     } else {
-        info = 'GetMobileInfo' ;
+        info = 'OpenWebPage:http://www.naver.com' ;
         alert(info);
         return info;
     }   
 }
+
+//1 : hashmap
+//2 : ArrayList
+//3 : 일반 스트링 배열 String[]
+
+function getMobileInfo1() {
+    if(ua.indexOf("Android") > 0) {
+      var info = window.bridge.setCommonData('GetMobileInfo1','') ; 
+       alert(info);
+       return info;
+    }
+    else if(ua.indexOf("iPhone") > 0) {
+        info = window.location = "GetMobileInfo1://?"; 
+        alert(info);
+        return  info;//+data;        
+    } else {
+        info = 'GetMobileInfo1' ;
+        alert(info);
+        return info;
+    }   
+}
+
+function getMobileInfo2() {
+    if(ua.indexOf("Android") > 0) {
+      var info = window.bridge.setCommonData('GetMobileInfo2','') ; 
+       alert(info);
+       return info;
+    }
+    else if(ua.indexOf("iPhone") > 0) {
+        info = window.location = "GetMobileInfo2://?"; 
+        alert(info);
+        return  info;//+data;        
+    } else {
+        info = 'GetMobileInfo2' ;
+        alert(info);
+        return info;
+    }   
+}
+
+function getMobileInfo3() {
+    if(ua.indexOf("Android") > 0) {
+      var info = window.bridge.setCommonData('GetMobileInfo3','') ; 
+       alert(info);
+       return info;
+    }
+    else if(ua.indexOf("iPhone") > 0) {
+        info = window.location = "GetMobileInfo3://?"; 
+        alert(info);
+        return  info;//+data;        
+    } else {
+        info = 'GetMobileInfo3' ;
+        alert(info);
+        return info;
+    }   
+}
+
